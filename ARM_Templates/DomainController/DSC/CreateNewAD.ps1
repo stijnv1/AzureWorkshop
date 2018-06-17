635c8a5189303dfbc718d41d02cb2345dc7d3e67
Stijn Verhoeven <Stijn.Verhoeven@realdolmen.com>
2018-06-17 16:02:12 +0200

    updated dsc configuration for dc: added creation of servers ou

---
+++ ARM_Templates/DomainController/DSC/CreateNewAD.ps1
@@ -0,0 +1,58 @@
+﻿configuration CreateNewAD 
+{ 
+   param 
+    ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds
+    ) 
+    
+    Import-DscResource -ModuleName xActiveDirectory, xPendingReboot
+    
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+   
+    Node localhost
+    {
+        LocalConfigurationManager            
+        {            
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true            
+        }
+
+        WindowsFeature ADDSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+        }
+
+        xADDomain CreateTestDomain
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+        }
+
+        xWaitForADDomain WaitForTestDomain
+        {
+            DomainName = $DomainName
+        }
+
+        xPendingReboot Reboot1
+        { 
+            Name = "RebootServer"
+            DependsOn = "[xWaitForADDomain]WaitForTestDomain"
+        }
+
+        xADOrganizationalUnit ServersOU
+        {
+            Name = "Servers"
+            Path = "DC=$($DomainName.Split('.')[0]),DC=$($DomainName.Split('.')[1])"
+            Credential = $DomainCreds
+            DependsOn = "[xPendingReboot]Reboot1"
+        }
+
+   }
+} 
